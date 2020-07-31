da21e0e8f793f4d9b875ed376fa13e4d85e4b65b
JOHN <boriskashtanov80@gmail.com>
2020-07-31 19:37:06 +0400

    added pagination

--- app/controllers/CategoryController.php
+++ app/controllers/CategoryController.php
@@ -7,6 +7,7 @@ namespace app\controllers;
 use app\models\Breadcrumbs;
 use app\models\Category;
 use ishop\App;
+use ishop\libs\Pagination;
 
 
 class CategoryController extends AppController
@@ -19,16 +20,18 @@ class CategoryController extends AppController
             throw new \Exception('Страница не найдена', 404);
         }
 
-        $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
-        $perpage = App::$app->getProperty('pagination');
-        // хлебные крошки
         $breadcrumbs = Breadcrumbs::getBreadcrumbs($category->id);
 
         $cat_model = new Category();
         $ids = $cat_model->getIds($category->id);
         $ids = !$ids ? $category->id : $ids . $category->id;
-        $products = \R::find('product', "category_id IN ($ids)");
+        $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+        $perpage = App::$app->getProperty('pagination');
+        $total = \R::count('product', "category_id IN ($ids)");
+        $pagination = new Pagination($page, $perpage, $total);
+        $start = $pagination->getStart();
+        $products = \R::find('product', "category_id IN ($ids) LIMIT $start, $perpage");
         $this->setMeta($category->title, $category->description, $category->keywords);
-        $this->set(compact('products', 'breadcrumbs'));
+        $this->set(compact('products', 'breadcrumbs', 'pagination', 'total'));
     }
 }
