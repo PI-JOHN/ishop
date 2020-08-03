f400049ef431b49c40542749cb5754073b0bf9d7
JOHN <boriskashtanov80@gmail.com>
2020-08-03 09:06:15 +0400

    added sending mail to client and to admin about order

--- config/params.php
+++ config/params.php
@@ -1,9 +1,12 @@
 <?php
 
 return [
-    'admin_email' => 'admin@mail.com',
+    'admin_email' => 'boryaglazz@yandex.ru',
     'shop_name' => 'Магазин IShop',
     'pagination' => 3,
-    'smtp_login' => '',
-    'smtp_password' => '',
+    'smtp_host' => 'smtp.yandex.ru',
+    'smtp_port' => '465',
+    'smtp_protocol' => 'ssl',
+    'smtp_login' => 'admin@yandex.ru',
+    'smtp_password' => '111111',
 ];
